4b989d36ce0042ebd3bd4c07887ecf4fdb532934
Pikachuxxxx <Phani.s2909@icloud.com>
2023-10-14 07:37:19 +0530

    Added constexpr_map

--- include/RZSTL/map.h
+++ include/RZSTL/map.h
@@ -3,13 +3,15 @@
 
 #include "RZSTL/config.h"
 
+#include <EASTL/map.h>
+
 namespace Razix {
     // Namespace to switch C++ STL with other STLs
     namespace rzstl {
 
 #if USE_EASTL
-        template<typename T>
-        using map = eastl::map<T>;
+        template<typename Key, typename T, typename Compare = eastl::less<Key>, typename Allocator = EASTLAllocatorType>
+        using map = eastl::map<Key, T, Compare, Allocator>;
 #elif
 
         class map
--- include/RZSTL/string.h
+++ include/RZSTL/string.h
@@ -21,6 +21,23 @@ namespace Razix {
             return *lhs == *rhs && (*lhs == '\0' || str_equal(lhs + 1, rhs + 1));
         }
 
+        // Use different constants for 32 bit vs. 64 bit size_t
+        constexpr std::size_t hash_offset =
+            std::conditional_t<sizeof(std::size_t) < 8,
+                std::integral_constant<uint32_t, 0x811C9DC5>,
+                std::integral_constant<uint64_t, 0xCBF29CE484222325> >::value;
+        constexpr std::size_t hash_prime =
+            std::conditional_t<sizeof(std::size_t) < 8,
+                std::integral_constant<uint32_t, 0x1000193>,
+                std::integral_constant<uint64_t, 0x100000001B3> >::value;
+
+        // FNV-1a hash
+        constexpr static std::size_t str_hash(const char* str,
+            const std::size_t                             value = hash_offset) noexcept
+        {
+            return *str ? str_hash(str + 1, (value ^ static_cast<std::size_t>(*str)) * hash_prime) : value;
+        }
+
         class string
         {
         private:
@@ -57,6 +74,14 @@ namespace Razix {
                 return data;
             }
         };
+
+        template<>
+        struct hash< ::rzstl::string>
+        {
+            constexpr std::size_t operator()(const ::rzstl::string& str) const
+            {
+                return str_hash(str.data());
+            }
 #endif
     }    // namespace rzstl
 }    // namespace Razix
